6618576ca11824c17aba92d1aaa850a8468d68d3
Daisuke Yamashita <yamashita_d@itg.hitachi.co.jp>
2017-11-06 09:53:52 +0900

    commit refs #379 2017年

--- push.sh
+++ push.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 USER="h-ci-user01"
-PW="6BBmcKCF!"
+PW=""
 
 echo $USER
 echo $PW
 
-git add .travis.yml
+git add -A
 git commit -m "commit refs #379 $1"
 git push -u origin master
 
-expect -c "
-set timeout 5
-spawn /bin/git push -u origin master
-expect \"Username for 'https://github.com':\"
-send \"${USER}\n\"
-expect \"Username for 'Password for 'https://h-ci-user01@github.com':\"
-send \"${PW}\n\"
-expect \"$\"
-exit 0
+#expect -c "
+#set timeout 5
+#spawn /bin/git push -u origin master
+#expect \"Username for 'https://github.com':\"
+#send \"${USER}\n\"
+#expect \"Username for 'Password for 'https://h-ci-user01@github.com':\"
+#send \"${PW}\n\"
+#expect \"$\"
+#exit 0
 
